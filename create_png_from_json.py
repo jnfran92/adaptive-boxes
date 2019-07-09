2b78e37358cc30f1cd2f9f099f1ea606d27e3ae1
Juan Chango <jchango@udlanet.ec>
2019-07-09 13:03:45 -0500

    improved plotting!

--- create_png_from_json.py
+++ create_png_from_json.py
@@ -3,19 +3,27 @@ import sys
 from lib.tools import load_from_json, Rectangle
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib
+import matplotlib.colors as colors
 plt.ioff()
 
 
 def plot_rectangles(recs_arg, sep_value_arg):
     max_area_val = np.max([item.get_area() for item in recs_arg])
 
-    plt.figure()
+    fig = plt.figure()
+    plt.axis('off')
+    ax = fig.add_subplot(111)
+
     sep_to_plot = sep_value_arg / 2
     for rec_val in recs_arg:
-        plot_rectangle(rec_val, sep_to_plot, max_area_val)
+        plot_rectangle(rec_val, sep_to_plot, max_area_val, ax)
+
+    plt.axis('scaled')
+    fig.tight_layout()
 
 
-def plot_rectangle(rec_arg, sep_to_plot_arg, max_area_arg):
+def plot_rectangle(rec_arg, sep_to_plot_arg, max_area_arg, ax):
     p1 = np.array([rec_arg.x1 - sep_to_plot_arg, rec_arg.y1 - sep_to_plot_arg])
     p2 = np.array([rec_arg.x1 - sep_to_plot_arg, rec_arg.y2 + sep_to_plot_arg])
     p3 = np.array([rec_arg.x2 + sep_to_plot_arg, rec_arg.y1 - sep_to_plot_arg])
@@ -27,7 +35,10 @@ def plot_rectangle(rec_arg, sep_to_plot_arg, max_area_arg):
     max_log = np.log2(max_n + 1)
     area_ratio = (max_n*(rec_arg.get_area()/max_area_arg))+1
     line_w = np.log2(area_ratio)/max_log
-    plt.plot(ps[:, 0], ps[:, 1], linewidth=0.6*line_w + 0.2)
+    plt.plot(ps[:, 0], ps[:, 1], linewidth=0.2*line_w + 0.08, c='r')
+
+    rect = matplotlib.patches.Rectangle((p1[0], p1[1]), p3[0] - p1[0], p2[1] - p1[1], color='yellow', lw=0)
+    ax.add_patch(rect)
 
 
 if len(sys.argv) < 3:
@@ -36,6 +47,9 @@ if len(sys.argv) < 3:
 
 in_path = str(sys.argv[1])      # .json
 out_path = str(sys.argv[2])     # .jpg
+
+colors_list = list(colors._colors_full_map.values())
+colors_len = len(colors_list)
 
 json_data_raw = load_from_json(in_path)
 
@@ -49,5 +63,5 @@ for jd in json_data:
 
 
 plot_rectangles(recs, sep_value)
-plt.savefig(out_path, dpi=1400)
+plt.savefig(out_path, dpi=2000)
 
