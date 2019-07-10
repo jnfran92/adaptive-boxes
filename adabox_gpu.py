6af972f338bc171309136ba327d26da24bc60a7b
Juan Chango <jchango@udlanet.ec>
2019-07-10 17:31:12 -0500

    make som room, and colors changed!

--- adabox_gpu.py
+++ adabox_gpu.py
@@ -43,10 +43,62 @@ plt.imshow(matmul)
 ax.set_aspect('equal')
 
 
-a = np.array([[1, 1, 1],
+a = np.array([[1, 0, 1],
               [1, 1, 1],
               [1, 1, 1],
               ])
 
-r = a.dot(a)
+b = np.array([[1, 1, 1],
+              [1, 1, 1],
+              [1, 1, 0],
+              ])
+
+r = a.dot(b)
+
+
+# Flatten Matrix
+data_matrix_f = data_matrix.flatten()
+
+# Kernel Data
+
+dim3_block_x = data_matrix.shape[0]
+dim3_block_y = data_matrix.shape[1]
+
+block_dim_y = dim3_block_y
+block_dim_x = dim3_block_x
+
+
+# KERNEL
+# Kernel non-editable - they go in for-loop
+block_idx_x = 0
+block_idx_y = 0
+
+thread_idx_x = 0
+thread_idx_y = 0
+
+# Kernel editable
+i = thread_idx_y
+j = thread_idx_x
+
+g_i = block_dim_y * block_idx_y + i
+g_j = block_dim_x * block_dim_x + j
+
+m = block_dim_y
+n = block_dim_x
+
+idx_i = 0
+idx_j = 1
+
+mat_val = data_matrix_f[m*i + j]
+
+distance_j = abs(g_j - j)
+
+
+# if mat_val == 0:
+#     if idx_j > j_g:
+#         print('idx_j is at left')
+#     else:
+#         print('idx_j is at right')
+
+
 
