c999b6a678b23e8390145b7b4a58f70f9ca74928
Juan Francisco Chango <jchango@udlanet.ec>
2020-04-29 01:05:45 -0400

    added new prepros file!

--- postproc_gpu/post_process_csv_gpu.py
+++ postproc_gpu/post_process_csv_gpu.py
@@ -12,7 +12,7 @@ from postproc_gpu.tools import create_groups, get_xy_units
 
 colors_list = list(colors._colors_full_map.values())
 
-in_path = "/Users/Juan/django_projects/adaptive-boxes/postproc_gpu/gpu_csv/humboldt.csv"  # .csv
+in_path = "/Users/Juan/Desktop/Tesis Model/partitions/humboldt_part.csv"  # .csv
 out_path = "/Users/Juan/django_projects/adaptive-boxes/postproc_gpu/csv_out_data/"  # without extension
 
 data = np.array(pd.read_csv(in_path, header=None))
