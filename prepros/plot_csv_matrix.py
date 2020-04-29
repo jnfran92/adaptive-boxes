697a75af36dd50ccc1528cafe60e6e5b182f9b5d
Juan Francisco Chango <jchango@udlanet.ec>
2020-04-29 00:04:04 -0400

    added new prepros file!

--- prepros/plot_csv_matrix.py
+++ prepros/plot_csv_matrix.py
@@ -1,12 +1,8 @@
 
 import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
 from numpy import genfromtxt
 
 data = genfromtxt('/Users/Juan/Desktop/Tesis Model/prepros/humboldt_binary_matrix.csv', delimiter=',')
-
-# plt.scatter(data)
 
 plt.subplot(111)
 plt.imshow(data, cmap='Greys',  interpolation='nearest')
