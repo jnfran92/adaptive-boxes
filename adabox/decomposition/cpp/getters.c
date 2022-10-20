90884411232dfd73a2c67335b2555c85e4b2078c
kolibri <jnfran92@gmail.com>
2022-10-19 23:56:38 -0300

    adding getters.c and lib so

---
+++ adabox/decomposition/cpp/getters.c
@@ -0,0 +1,169 @@
+
+#include "stdio.h"
+#include "stdlib.h"
+
+int get_bottom_distance(int idx_i_arg, int idx_j_arg, int n_arg, int lim, int *data_matrix_arg){
+    int di =0;
+    int temp_val = 0;
+    for (int i=idx_i_arg; i<lim; i++){
+        temp_val = data_matrix_arg[i * n_arg + idx_j_arg];
+        if(temp_val == 0){
+            break;
+        }
+        di++;
+    }
+    return di;
+}
+
+int get_top_distance(int idx_i_arg, int idx_j_arg, int n_arg, int lim, int *data_matrix_arg){
+    int di = 0;
+    int temp_val = 0;
+    for (int i=idx_i_arg; i>lim; i--){
+        temp_val = data_matrix_arg[i * n_arg + idx_j_arg];
+        if(temp_val == 0){
+            break;
+        }
+        di++;
+    }
+    return di;
+}
+
+// results matrix: [x1 x2 y1 y2]
+void get_right_bottom_rectangle(int idx_i_arg, int idx_j_arg, long m_arg, long n_arg, int *data_matrix_arg, int *results){
+
+    int x1_val = 0;
+    int x2_val = 0;
+    int y1_val = 0;
+    int y2_val = 0;
+
+    int d0, dj;
+
+    d0 = get_bottom_distance( idx_i_arg, idx_j_arg, n_arg,  m_arg, data_matrix_arg);
+
+    dj = 0;
+    for (int j=idx_j_arg + 1; j<n_arg; j++){
+        int di = get_bottom_distance(idx_i_arg, j, n_arg, idx_i_arg + d0, data_matrix_arg);
+        if (di < d0){
+            break;
+        }
+        dj++;
+    }
+
+    x1_val = idx_j_arg;
+    y1_val = idx_i_arg;
+    x2_val =  idx_j_arg + dj;
+    y2_val =  idx_i_arg + d0 - 1;
+
+    results[0] = x1_val;
+    results[1] = x2_val;
+    results[2] = y1_val;
+    results[3] = y2_val;
+
+}
+
+
+
+void get_left_bottom_rectangle(int idx_i_arg, int idx_j_arg, long m_arg, long n_arg, int *data_matrix_arg, int *results){
+
+
+    int x1_val = 0;
+    int x2_val = 0;
+    int y1_val = 0;
+    int y2_val = 0;
+
+
+    int d0,dj;
+
+    d0 = get_bottom_distance( idx_i_arg, idx_j_arg, n_arg, m_arg, data_matrix_arg);
+    dj = 0;
+    for (int j=idx_j_arg - 1; j>=0; j--){
+
+        int di = get_bottom_distance( idx_i_arg, j, n_arg, idx_i_arg + d0, data_matrix_arg);
+
+        if (di < d0){
+            break;
+        }
+        dj++;
+    }
+
+    x1_val = idx_j_arg;
+    y1_val = idx_i_arg;
+    x2_val = idx_j_arg - dj;
+    y2_val = idx_i_arg + d0 - 1;
+
+
+    results[0] = x1_val;
+    results[1] = x2_val;
+    results[2] = y1_val;
+    results[3] = y2_val;
+}
+
+
+
+void get_left_top_rectangle(int idx_i_arg, int idx_j_arg, long n_arg, int *data_matrix_arg, int *results){
+
+
+    int x1_val = 0;
+    int x2_val = 0;
+    int y1_val = 0;
+    int y2_val = 0;
+
+
+    int d0, dj;
+
+    d0 = get_top_distance( idx_i_arg, idx_j_arg, n_arg, -1, data_matrix_arg);
+    dj = 0;
+    for (int j=idx_j_arg - 1; j>-1; j--){
+
+        int di = get_top_distance( idx_i_arg, j, n_arg, idx_i_arg - d0, data_matrix_arg);
+        if (di < d0){
+            break;
+        }
+        dj++;
+    }
+
+    x1_val = idx_j_arg;
+    y1_val = idx_i_arg;
+    x2_val = idx_j_arg - dj;
+    y2_val = idx_i_arg - d0 + 1;
+
+
+    results[0] = x1_val;
+    results[1] = x2_val;
+    results[2] = y1_val;
+    results[3] = y2_val;
+}
+
+
+void get_right_top_rectangle(int idx_i_arg, int idx_j_arg, long n_arg, int *data_matrix_arg, int *results){
+
+
+    int x1_val = 0;
+    int x2_val = 0;
+    int y1_val = 0;
+    int y2_val = 0;
+
+
+    int d0, dj;
+
+    d0 = get_top_distance( idx_i_arg, idx_j_arg, n_arg, -1, data_matrix_arg);
+    dj = 0;
+    for (int j=idx_j_arg + 1; j<n_arg; j++){
+        int di = get_top_distance( idx_i_arg, j, n_arg, idx_i_arg - d0, data_matrix_arg);
+        if (di < d0){
+            break;
+        }
+        dj++;
+    }
+
+    x1_val = idx_j_arg;
+    y1_val = idx_i_arg;
+    x2_val = idx_j_arg + dj;
+    y2_val = idx_i_arg - d0 + 1;
+
+
+    results[0] = x1_val;
+    results[1] = x2_val;
+    results[2] = y1_val;
+    results[3] = y2_val;
+}
--- sample_data/sample.py
+++ sample_data/sample.py
@@ -5,11 +5,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Input Path
-in_path = './sample_data/boston12.csv'
+in_path = './sample_data/humboldt_binary_matrix.csv'
 
 # Load Demo data with columns [x_position y_position flag]
 binary_matrix = np.loadtxt(in_path, delimiter=",")
 
 
 # Plot demo data
-plt.imshow(binary_matrix, cmap='Greys',  interpolation='nearest')
+plt.imshow(np.flip(binary_matrix, axis=0), cmap='magma',  interpolation='nearest')
