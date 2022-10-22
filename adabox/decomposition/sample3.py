143f0df9e146c918ce317a9e53f7736ff8579868
kolibri <jnfran92@gmail.com>
2022-10-22 16:08:19 -0300

    adabox sample completed on sample3.py

--- adabox/decomposition/sample3.py
+++ adabox/decomposition/sample3.py
@@ -1,8 +1,6 @@
-
 import ctypes
 import random
 from timeit import default_timer as timer
-import matplotlib.pyplot as plt
 import numpy as np
 
 from adabox.plot_tools import plot_rectangles
@@ -11,16 +9,24 @@ from adabox.tools import Rectangle
 
 def find_a_rectangle(point, data_binary_matrix, so_lib):
     c_int_p = ctypes.POINTER(ctypes.c_int)
+    c_double_p = ctypes.POINTER(ctypes.c_double)
+
     data_matrix_ptr = data_binary_matrix.ctypes.data_as(c_int_p)
     out = np.array([0, 0, 0, 0]).astype(np.intc)
     out_ptr = out.ctypes.data_as(c_int_p)
 
-    idx_var = int(point[0][0])
-    idj_var = int(point[0][1])
+    area = np.array([0]).astype(np.intc)
+    area_ptr = area.ctypes.data_as(c_int_p)
+
+    ab_ratio = np.array([0]).astype(np.float64)
+    ab_ratio_ptr = ab_ratio.ctypes.data_as(c_double_p)
+
+    idx_var = int(point[0])
+    idj_var = int(point[1])
     m = data_binary_matrix.shape[0]
     n = data_binary_matrix.shape[1]
-    so_lib.find_largest_rectangle(idx_var, idj_var, m, n, data_matrix_ptr, out_ptr)
-    return out
+    so_lib.find_largest_rectangle(idx_var, idj_var, m, n, data_matrix_ptr, out_ptr, area_ptr, ab_ratio_ptr)
+    return out, area, ab_ratio
 
 
 def remove_rectangle_from_matrix(rec_to_remove, data_binary_matrix):
@@ -31,7 +37,21 @@ def remove_rectangle_from_matrix(rec_to_remove, data_binary_matrix):
     data_binary_matrix[x2:y2 + 1, x1:y1 + 1] = 0
 
 
-so_file = "/Users/kolibri/PycharmProjects/adaptive-boxes/adabox/decomposition/cpp/getters.so"
+def find_rectangles_and_filter_the_best(random_points_arg, data_matrix_arg, lib_arg):
+    results = []
+    for rp in random_points_arg:
+        rec, rec_area, ab_ratio = find_a_rectangle(rp, data_matrix_arg, lib_arg)
+        results.append([rec, rec_area, ab_ratio])
+
+    # conditions
+    results_array_area = np.array(results)[:, 1]
+    results_array_ab_side = np.array(results)[:, 2]
+
+    result = results[results_array_area.argmax()]
+    return result[0], result[1], result[2]
+
+
+so_file = "/Users/kolibri/PycharmProjects/adaptive-boxes/adabox/decomposition/cpp/getters_completed.so"
 getters_so_lib = ctypes.CDLL(so_file)
 
 # Input Path
@@ -48,29 +68,34 @@ data_matrix = data_matrix.astype(np.intc)
 # search rectangle
 coords = np.argwhere(data_matrix == 1)
 recs = []
+areas = []
+ab_ratios = []
 
 start = timer()
 while coords.shape[0] != 0:
-    print("searching...")
     start2 = timer()
-    random_point = random.choices(coords)
+    n_searches = 100
+    random_points = random.choices(coords, k=n_searches)
     end2 = timer()
-    print("elapsed time random point " + str((end2 - start2)*1000) + " milli-seconds")
+    print("elapsed time random point " + str((end2 - start2) * 1000) + " milli-seconds")
 
     start2 = timer()
-    rec = find_a_rectangle(random_point, data_matrix, getters_so_lib)
+    rec, rec_area, ab_ratio = find_rectangles_and_filter_the_best(random_points, data_matrix, getters_so_lib)
     remove_rectangle_from_matrix(rec, data_matrix)
     end2 = timer()
-    print("elapsed time random find/remove " + str((end2 - start2)*1000) + " milli-seconds")
+    print("elapsed time random find/remove " + str((end2 - start2) * 1000) + " milli-seconds")
 
+    start2 = timer()
     coords = np.argwhere(data_matrix == 1)
     recs.append(rec)
+    areas.append(rec_area)
+    ab_ratios.append(ab_ratio)
+    end2 = timer()
+    print("elapsed time random coords and appends " + str((end2 - start2) * 1000) + " milli-seconds")
+    print("")
 end = timer()
 print("elapsed time " + str(end - start) + "seconds")
-
 
 # Plotting
 rectangles_list = list(map(lambda x: Rectangle(x[0], x[1], x[2], x[3]), recs))
 plot_rectangles(rectangles_list, 1)
-
-
