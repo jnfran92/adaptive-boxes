d29330ddc7538cf334a8100d643a104e19ed29c2
jnfran92 <jnfran92@gmail..com>
2023-06-13 00:37:13 -0400

    updated complete files

--- adabox/complete/4_gexf_partitioned_to_gpu_device_global_data.py
+++ adabox/complete/4_gexf_partitioned_to_gpu_device_global_data.py
@@ -11,7 +11,6 @@ import pandas as pd
 colors_list = list(colors._colors_full_map.values())
 
 
-
 def create_folder(path_tmp_arg):
     if not os.path.isdir(path_tmp_arg):
         os.makedirs(path_tmp_arg)
